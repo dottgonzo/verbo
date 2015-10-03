63d9c097235617bfb1c9f86d22042837adb5c48c
Dario Caruso <darioyzf@gmail.com>
2015-10-03 02:18:08 +0200

    oki

---
+++ index.js
@@ -0,0 +1,26 @@
+var colors=require('colors');
+
+colors.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
+
+module.exports = function(string,action,area) {
+if(!action){
+  action='debug'
+}
+if(!area){
+  area='working'
+}
+  console.log((action+'@'+area)[action]+': '+string);
+
+}
+
