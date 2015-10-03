2a8ff9d3226aa76111e17cae5989acec8662dde8
Dario Caruso <darioyzf@gmail.com>
2015-10-03 03:55:40 +0200

    oki

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var colors=require('colors');
-colors.setTheme({
-  silly: 'rainbow',
-  input: 'grey',
-  verbose: 'cyan',
-  prompt: 'grey',
-  info: 'green',
-  data: 'grey',
-  help: 'cyan',
-  warn: 'yellow',
-  debug: 'blue',
-  error: 'red'
-});
+
 
 module.exports = function(string,action,area) {
+  var colors=require('colors');
+  colors.setTheme({
+    silly: 'rainbow',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+  });
 if(!action){
   action='debug'
 }
--- tests/main.js
+++ tests/main.js
@@ -1,1 +1,8 @@
-// test colors
+var verb=require('../index.js');
+
+verb("only string");
+
+
+verb("string info","info");
+
+verb("string info","info","area");
